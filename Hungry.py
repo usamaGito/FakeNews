0a656cdc4998372168d56f44ced457f5975198aa
Usama Fassy <usamafassy@gmail.com>
2022-03-14 20:51:10 +0000

    Project set up

--- Hungry.py
+++ Hungry.py
@@ -1,3 +1,4 @@
 print('I am Hungry')
 print('I am Hungry 2')
-Print (' I am hungry')
+Print('I am hungry')
+print('this is from Parmeet')
---
+++ liar_plus_dataset/tpot_exported_pipeline_count.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=42)
+
+# Average CV score on the training set was: 0.4112785490998697
+exported_pipeline = DecisionTreeClassifier(criterion="entropy", max_depth=6, min_samples_leaf=1, min_samples_split=16)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 42)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
